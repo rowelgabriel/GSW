8facd030eec8a323de100c231fb40af3762c2017
Rowel Gabriel <rowelgabrielvmangabat@gmail.com>
2025-11-25 15:21:27 +0800

    js added

---
+++ data.js
@@ -0,0 +1,10 @@
+var p_data = [
+  {
+    "name": "Stephen Curry",
+    "number": "30",
+    "position": "Point Guard",
+    "height": "191 cm",
+    "image": "images/curry.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  }
+]
