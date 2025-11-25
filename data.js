c49710c1dadbed1018a81ccb0cf5299e1c912e0c
Rowel Gabriel <rowelgabrielvmangabat@gmail.com>
2025-11-25 16:11:09 +0800

    gallery updated

--- data.js
+++ data.js
@@ -1,10 +1,130 @@
 var p_data = [
   {
+    "name": "Gary Payton II",
+    "number": "0",
+    "position": "Shooting Guard",
+    "height": "188 cm",
+    "image": "images/payton.jpg",
+    "stats": "https://www.nba.com/stats/player/1627780"
+  },
+  {
+    "name": "Damion Lee",
+    "number": "1",
+    "position": "Shooting Guard",
+    "height": "195 cm",
+    "image": "images/lee.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
+    "name": "Chris Chiozza",
+    "number": "2",
+    "position": "Point Guard",
+    "height": "183 cm",
+    "image": "images/chiozza.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
+    "name": "Jordan Poole",
+    "number": "3",
+    "position": "Shooting Guard",
+    "height": "193 cm",
+    "image": "images/poole.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
+    "name": "Moses Moody",
+    "number": "4",
+    "position": "Shooting Guard",
+    "height": "196 cm",
+    "image": "images/moody.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
+    "name": "Kevon Looney",
+    "number": "5",
+    "position": "Center",
+    "height": "206 cm",
+    "image": "images/looney.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
+    "name": "Nemanja Bjelica",
+    "number": "8",
+    "position": "Small Forward",
+    "height": "209 cm",
+    "image": "images/bjelica.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
+    "name": "Andre Iguodala",
+    "number": "9",
+    "position": "Small Forward",
+    "height": "198 cm",
+    "image": "images/iguodala.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
+    "name": "Klay Thompson",
+    "number": "11",
+    "position": "Shooting Guard",
+    "height": "196 cm",
+    "image": "images/thompson.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
+    "name": "Andrew Wiggins",
+    "number": "22",
+    "position": "Small Forward",
+    "height": "201 cm",
+    "image": "images/wiggins.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
+    "name": "Draymond Green",
+    "number": "23",
+    "position": "Power Forward",
+    "height": "198 cm",
+    "image": "images/green.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
     "name": "Stephen Curry",
     "number": "30",
     "position": "Point Guard",
     "height": "191 cm",
     "image": "images/curry.jpg",
     "stats": "https://www.nba.com/stats/player/201939"
-  }
+  },
+  {
+    "name": "Otto Porter Jr.",
+    "number": "32",
+    "position": "Power Forward",
+    "height": "203 cm",
+    "image": "images/porter.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
+    "name": "James Wiseman",
+    "number": "33",
+    "position": "Center",
+    "height": "211 cm",
+    "image": "images/wiseman.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
+    "name": "Juan Toscano-Anderson",
+    "number": "95",
+    "position": "Power Forward",
+    "height": "198 cm",
+    "image": "images/toscano.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
+  {
+    "name": "Jonathan Kuminga",
+    "number": "00",
+    "position": "Power Forward",
+    "height": "201 cm",
+    "image": "images/kuminga.jpg",
+    "stats": "https://www.nba.com/stats/player/201939"
+  },
 ]
